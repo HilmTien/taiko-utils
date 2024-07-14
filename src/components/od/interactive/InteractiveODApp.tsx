2bd6317173d025f3d901408864afc1ad3339c9ed
HilmTien <tient2477@gmail.com>
2024-07-14 23:01:52 +0200

    :sparkles: add mod buttons to gui

--- src/components/od/interactive/InteractiveODApp.tsx
+++ src/components/od/interactive/InteractiveODApp.tsx
@@ -3,6 +3,8 @@
 import { Slider } from "@/components/ui/atoms/slider";
 import React from "react";
 
+import ModButton from "@/components/ui/molecules/ModButton";
+
 export default function InteractiveODComponent() {
   let [od, setOD] = React.useState([5]);
 
@@ -16,6 +18,14 @@ export default function InteractiveODComponent() {
         max={10}
         step={0.01}
       />
+      <div className="w-auto flex justify-center pt-6">
+        <div className="grid grid-cols-2 gap-2 place-content-center place-items-center select-none">
+          <ModButton mod="ez" />
+          <ModButton mod="ht" />
+          <ModButton mod="hr" />
+          <ModButton mod="dt" />
+        </div>
+      </div>
     </>
   );
 }
