d405400e30c8b103ed3d6b086794ba38d897d541
HilmTien <tient2477@gmail.com>
2024-07-14 22:59:14 +0200

    :sparkles: add icon import file with type

---
+++ src/lib/modIcons.ts
@@ -0,0 +1,8 @@
+import dt from "../../public/mod-badges/dt.png";
+import ez from "../../public/mod-badges/ez.png";
+import hr from "../../public/mod-badges/hr.png";
+import ht from "../../public/mod-badges/ht.png";
+
+export type ODAdjustingMod = "ez" | "ht" | "hr" | "dt";
+
+export default { dt, ez, hr, ht };
