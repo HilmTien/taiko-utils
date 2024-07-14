768c331316c9560e4fad2255e29de67f87d0f8b5
HilmTien <tient2477@gmail.com>
2024-07-14 23:00:51 +0200

    :sparkles: add modbutton component

---
+++ src/components/ui/molecules/ModButton.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { ODDispatchContext, ODStateContext } from "@/components/od/ODState";
+import { ODAdjustingMod } from "@/lib/modIcons";
+import Image from "next/image";
+import React from "react";
+import { Button } from "../atoms/button";
+
+interface ModButtonProps {
+  mod: ODAdjustingMod;
+}
+
+export default function ModButton({ mod }: ModButtonProps) {
+  const state = React.useContext(ODStateContext);
+  const dispatch = React.useContext(ODDispatchContext);
+
+  return (
+    <Button
+      size={"icon"}
+      className="w-[92px] h-[68px] transition grayscale aria-[checked=true]:grayscale-0 hover:scale-105 aria-[checked=true]:rotate-[4deg]"
+      role="checkbox"
+      aria-checked={state.interactive.activeMods[mod]}
+      onClick={(_) => {
+        dispatch({ type: "interactiveModChanged", mod: mod });
+      }}
+    >
+      <Image
+        src={`/mod-badges/${mod}.png`}
+        width={92}
+        height={68}
+        alt={mod}
+        draggable={false}
+        priority={true}
+        className="h-full w-full"
+      />
+    </Button>
+  );
+}
