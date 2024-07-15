97bc1e451a00495af5aec6746072e9607c659222
HilmTien <tient2477@gmail.com>
2024-07-15 23:02:57 +0200

    :recycle: use new round util

--- src/lib/od/overallDifficulty.ts
+++ src/lib/od/overallDifficulty.ts
@@ -1,3 +1,6 @@
+import { ODAdjustingMod } from "../modIcons";
+import { round } from "../utils";
+
 export function ODtoMS300(od: number) {
   return Math.floor(49 - od * 3) + 0.5;
 }
@@ -12,4 +15,36 @@ export function ODtoMS150(od: number) {
 
 export function MStoOD150(ms: number) {
   return ms > 79 ? -ms / 8 + 239 / 16 : (-1 / 12) * (2 * ms - 219);
+}
+
+export function applyMods(
+  od: number,
+  activeMods: Record<ODAdjustingMod, boolean>
+) {
+  if (activeMods.ez) {
+    od /= 2;
+  } else if (activeMods.hr) {
+    od *= 1.4;
+    if (od > 10) {
+      od = 10;
+    }
+  }
+
+  od = round(od, 2);
+  let ms300 = ODtoMS300(od);
+  let ms150 = ODtoMS150(od);
+
+  if (activeMods.ht) {
+    ms300 *= 4 / 3;
+    ms150 *= 4 / 3;
+  } else if (activeMods.dt) {
+    ms300 *= 2 / 3;
+    ms150 *= 2 / 3;
+  }
+
+  // highest OD that fits the ms300/ms150 boundaries
+  let odMax300 = MStoOD300(ms300);
+  let odMax150 = MStoOD150(ms150);
+
+  return { ms300, ms150, odMax300, odMax150 };
 }
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,7 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function round(value: number, decimals: number = 2) {
+  return Math.round(value * Math.pow(10, decimals)) / Math.pow(10, decimals);
+}
