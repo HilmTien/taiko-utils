ae76bd1ed0c036b1a28c76269697e31a124336c5
Tien <tient2477@gmail.com>
2024-08-07 16:25:57 +0200

    :bug: add runtimeerror handling

--- src/app/profile-adjustment/[id]/page.tsx
+++ src/app/profile-adjustment/[id]/page.tsx
@@ -2,6 +2,7 @@ import { getData } from "@/lib/utils";
 
 import ProfileAdjustmentApp from "@/components/profile-adjustment/ProfileAdjustmentApp";
 import { ProfileAdjustmentStateProvider } from "@/components/profile-adjustment/ProfileAdjustmentState";
+import { permanentRedirect } from "next/navigation";
 
 interface PageProps {
   params: { id: string };
@@ -11,6 +12,10 @@ export default async function Page({ params: { id } }: PageProps) {
   const data: Array<any> = await getData(
     `https://osu.ppy.sh/users/${id}/scores/best?mode=taiko&limit=100`
   );
+
+  if (!data) {
+    permanentRedirect("/profile-adjustment");
+  }
 
   return (
     <main className="p-8">
