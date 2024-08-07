72d0629f90911c539f477a5ec58b0942d8c58576
Tien <tient2477@gmail.com>
2024-08-07 16:25:20 +0200

    :sparkles: add basic index page

---
+++ src/components/profile-adjustment/ProfileAdjustmentIndex.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import React from "react";
+import { Button } from "../ui/atoms/Button";
+import InputField from "../ui/molecules/InputField";
+
+export default function ProfileAdjustmentIndex() {
+  const router = useRouter();
+
+  const [userID, setUserID] = React.useState(0);
+
+  const onUserIDChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setUserID(
+      e.target.value
+        ? parseInt(e.target.value) > 0
+          ? parseInt(e.target.value)
+          : 0
+        : 0
+    );
+  };
+
+  const onUserIDSubmit = async () => {
+    if (!Number.isNaN(userID)) {
+      router.push(`profile-adjustment/${userID.toString()}`);
+    }
+  };
+
+  return (
+    <div>
+      <InputField
+        label={"User ID:"}
+        type="number"
+        step={1}
+        value={userID.toString()}
+        onChange={onUserIDChange}
+      />
+      <Button variant="default" size="default" onClick={onUserIDSubmit}>
+        Submit
+      </Button>
+    </div>
+  );
+}
