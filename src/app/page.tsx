b7c5a10ab94dbdc96dfc800007cfa3b50d0800c6
Tien <tient2477@gmail.com>
2024-08-07 23:44:20 +0200

    :sparkles: add simple link to profile adjustment app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,9 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main>
+    <main className="flex flex-col gap-2">
       <Link href={"/od"}>Go to OD app</Link>
+      <Link href={"/profile-adjustment"}>Go to Profile Adjustment app</Link>
     </main>
   );
 }
