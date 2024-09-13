3b5f6b6da25af2e6e9c5213d8554d195cf7ad482
Tien <tient2477@gmail.com>
2024-09-13 19:15:58 +0200

    :sparkles: add more mods

--- src/lib/modIcons.ts
+++ src/lib/modIcons.ts
@@ -1,8 +1,11 @@
 import dt from "../../public/mod-badges/dt.png";
 import ez from "../../public/mod-badges/ez.png";
+import fl from "../../public/mod-badges/fl.png";
+import hd from "../../public/mod-badges/hd.png";
 import hr from "../../public/mod-badges/hr.png";
 import ht from "../../public/mod-badges/ht.png";
 
 export type ODAdjustingMod = "ez" | "ht" | "hr" | "dt";
+export type Mod = "ez" | "hr" | "dt" | "ht" | "hd" | "fl";
 
-export default { dt, ez, hr, ht };
+export default { dt, ez, hr, ht, hd, fl };
