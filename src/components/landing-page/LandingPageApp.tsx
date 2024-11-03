0be8c430e617dad06b5c29abc0e87dbcbeaaccfe
TonyWorep <tonyworep@gmail.com>
2024-11-03 23:14:10 +0100

    :lipstick: completed the od app section

--- src/components/landing-page/LandingPageApp.tsx
+++ src/components/landing-page/LandingPageApp.tsx
@@ -2,6 +2,7 @@
 
 import { ChevronRight } from "lucide-react";
 import Image from "next/image";
+import odGraph from "public/od-graph.png";
 import taiko from "public/taiko.png";
 import { Button } from "../ui/atoms/Button";
 
@@ -17,7 +18,7 @@ export default function LandingPageApp() {
         </p>
       </div>
 
-      <div className="flex flex-col gap-2 bg-secondary pl-[15%] pt-40">
+      <div className="flex flex-col gap-2 bg-secondary pl-[15%] pt-40 h-[500px]">
         <h1 className="text-4xl font-extrabold">OD after mod change?</h1>
         <p className="max-w-[700px] text-violet-200 mb-5">
           The OD application gives you the ability to find the correct "Overall
@@ -26,10 +27,15 @@ export default function LandingPageApp() {
           application also includes a table and a graph to give you a
           visualization as well.
         </p>
-        <Button variant={"outline"} size={"default"} className="h-12 w-32">
-          Try OD App
-          <ChevronRight />
-        </Button>
+        <div className="flex gap-64">
+        <a href={"/od"} className="h-12 w-32">
+          <Button variant={"outline"} size={"default"} className="h-12 w-32">
+            Try OD App
+            <ChevronRight />
+          </Button>
+        </a>
+        <Image src={odGraph} alt="od graph" className="rounded-lg w-[500px] border shadow-2xl"/>
+        </div>
       </div>
     </main>
   );
