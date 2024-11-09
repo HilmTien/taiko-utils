feaed31d5370aed2005c423b7546acf0682f60c2
TonyWorep <tonyworep@gmail.com>
2024-11-09 17:37:35 +0100

    :lipstick: change hall of fame

--- src/components/general/footer/Footer.tsx
+++ src/components/general/footer/Footer.tsx
@@ -8,7 +8,7 @@ export default function Footer() {
   return (
     <footer className="flex mt-52 mb-10">
       <div className="flex flex-col mx-auto">
-        <div className="flex mx-auto gap-[750px]">
+        <div className="flex mx-auto gap-[770px]">
           <Link href="https://vercel.com" target="_blank">
             <Image src={vercel} alt="vercel" className="w-40" />
           </Link>
@@ -16,15 +16,17 @@ export default function Footer() {
             <Image src={github} alt="github" className="w-10" />
           </Link>
         </div>
-        <Separator className="mx-auto my-5" />
-        <div className="flex gap-[650px] mx-auto place-items-center">
+        <Separator className="mx-auto my-3" />
+        <div className="flex gap-[450px] mx-auto place-items-center">
           <span className="flex place-items-baseline gap-2">
             <h1 className="font-mono font-extrabold text-3xl">TAIKO UTILS</h1>
             <p className="font-mono font-extralight text-violet-200">
-              {new Date().getFullYear()}
+              Copyright &copy; {new Date().getFullYear()}
             </p>
           </span>
-          <p>Hall of Fame</p>
+          <p>
+            <b>Defectum</b> & <b>TonyWorep</b>
+          </p>
         </div>
       </div>
     </footer>
