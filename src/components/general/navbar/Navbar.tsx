101f559cfe06bcdcfc23a908a758e15b19b96d07
TonyWorep <tonyworep@gmail.com>
2024-11-25 20:10:52 +0100

    :fire: Removed useless code

--- src/components/general/navbar/Navbar.tsx
+++ src/components/general/navbar/Navbar.tsx
@@ -1,27 +1,13 @@
 "use client";
 
 import { NavigationMenu } from "@/components/ui/atoms/NavigationMenu";
-import useSession from "@/lib/hooks/useSession";
-import { fetcher } from "@/lib/utils";
 import { HomeIcon } from "@heroicons/react/24/outline";
 import Link from "next/link";
-import { UserExtended } from "osu-web.js";
-import useSWR from "swr";
 import NavApplications from "./nav-lists/NavApplications";
 import NavMenu from "./NavMenu";
 import ProfileHandler from "./profile/ProfileHandler";
 
 export default function Navbar() {
-  const res = useSWR<UserExtended>("/api/osu/get-self", fetcher);
-  const data = res.data;
-
-  const { logout } = useSession();
-
-  const onLogout = async () => {
-    await logout();
-    location.reload();
-  };
-
   return (
     <header className="h-14 w-full top-0 bg-navbar flex justify-between">
       <div className="mx-auto w-[calc(100%-50px)] h-full max-w-screen-lg flex items-center gap-5">
