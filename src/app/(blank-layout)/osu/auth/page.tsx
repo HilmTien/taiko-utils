23f60a155cd7d2e71b65113f21dd2144f6fa040d
HilmTien <tient2477@gmail.com>
2025-03-02 01:43:38 +0100

    fix: auth for custom domains

--- src/app/(blank-layout)/osu/auth/page.tsx
+++ src/app/(blank-layout)/osu/auth/page.tsx
@@ -3,8 +3,11 @@
 import AuthClient from "@/components/auth-client/AuthClient";
 import { OsuToken } from "@/lib/session";
 import { getData } from "@/lib/utils";
+import { headers } from "next/headers";
 
 export default async function Page({ searchParams }: { searchParams: any }) {
+  const host = headers().get("host");
+
   const code = searchParams.code as string | undefined;
   if (!code) {
     return (
@@ -15,16 +18,20 @@ export default async function Page({ searchParams }: { searchParams: any }) {
     );
   }
 
-  const headers = {
+  const reqHeaders = {
     Accept: "application/json",
     "Content-Type": "application/x-www-form-urlencoded",
   };
 
-  const body = `client_id=${process.env.OSU_CLIENT_ID}&client_secret=${process.env.OSU_CLIENT_SECRET}&code=${code}&grant_type=authorization_code&redirect_uri=${process.env.HOST}/osu/auth`;
+  const body = `client_id=${process.env.OSU_CLIENT_ID}&client_secret=${
+    process.env.OSU_CLIENT_SECRET
+  }&code=${code}&grant_type=authorization_code&redirect_uri=${
+    process.env.NODE_ENV === "production" ? "https" : "http"
+  }://${host}/osu/auth`;
 
   const token = (await getData("https://osu.ppy.sh/oauth/token", {
     method: "POST",
-    headers: headers,
+    headers: reqHeaders,
     body: body,
   })) as OsuToken;
 
--- src/app/(blank-layout)/osu/login/page.tsx
+++ src/app/(blank-layout)/osu/login/page.tsx
@@ -1,11 +1,16 @@
 "use server";
 
+import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 
 export default async function Page() {
+  const host = headers().get("host");
+
   const params = new URLSearchParams({
     client_id: process.env.OSU_CLIENT_ID!,
-    redirect_uri: `${process.env.HOST}/osu/auth`,
+    redirect_uri: `${
+      process.env.NODE_ENV === "production" ? "https" : "http"
+    }://${host}/osu/auth`,
     response_type: "code",
     scope: "public identify",
   });
